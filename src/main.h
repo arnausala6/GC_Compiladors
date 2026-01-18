1ae500f230798059b77c4ac7f7dfc7b3a3eb4292
Ivanorf <166705299+Ivanorf@users.noreply.github.com>
2026-01-18 18:37:41 +0100

    include de la libreria del modulo 2 elimanado

--- src/main.h
+++ src/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include "./utils_files.h"
 #include "./module_args/module_args.h"
-#include "./module_2/module_2.h"
 #include "./module_preprocessor/preprocessor.h"
 
 // Output file of project run: either a stdout or a filename with log extension (comment one out)
