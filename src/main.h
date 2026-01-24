ce3ab84e96d31428b87eb09269922837c0a9957d
Ivanorf <166705299+Ivanorf@users.noreply.github.com>
2026-01-24 11:54:16 +0100

    deleted global variable ofile

--- src/main.h
+++ src/main.h
@@ -2,13 +2,10 @@
 #define MAIN_H
  
 #include <stdio.h>
-#include "./utils_files.h"
 #include "./module_args/module_args.h"
 
 // Output file of project run: either a stdout or a filename with log extension (comment one out)
 #define PROJOUTFILENAME "./proj_modules_template.log"
 //#define PROJOUTFILENAME "stdout"
-
-extern FILE* ofile; // The output handler for the project run (same variable name as in modules)
 
 #endif // MAIN_H
