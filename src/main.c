90c47c18b31c9c72af92eb6a4f99ae71756cd7f6
Ivanorf <166705299+Ivanorf@users.noreply.github.com>
2026-01-24 11:55:00 +0100

    main.c finished

--- src/main.c
+++ src/main.c
@@ -1,22 +1,23 @@
 #include "./main.h"
 
-FILE* ofile = NULL; // The output handler for the project run (same variable name as in modules)
-
 int main(int argc, char *argv[]) {
 
-    ofile = stdout; // Default output to stdout
-    ofile = set_output_test_file(PROJOUTFILENAME);
+    FILE *ofile = fopen(PROJOUTFILENAME, "w");
+    FILE *infile = fopen(argv[1], "r");
+    if(ofile == NULL) {
+        ofile = stdout;
+    }
 
     fprintf(ofile, "Starting module args ...\n");
     int mode = process_arguments(argc, argv);
     if(mode == -1) {
-        fprintf(ofile, "Error processing arguments. Exiting.\n");
+        printf("Error processing arguments. Exiting.\n");
         fclose(ofile);
         return 1;
     }
-    
-    motor_preprocesador(, , mode);
-    fprintf(ofile, "All modules executed successfully!\n\n");
+
+    motor_preprocesador(infile, ofile, mode);
+    printf("All modules executed successfully!\n\n");
 
     fclose(ofile); 
     return 0;
