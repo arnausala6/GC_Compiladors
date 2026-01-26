3b9dcef20a9374018dae685de055ad635c3a5638
Ivanorf <ivan.jimenez04@estudiant.upf.edu>
2026-01-26 16:40:43 +0100

    añadido modulo macrostoring

---
+++ src/macrostoring/macrostoring.h
@@ -0,0 +1,11 @@
+typedef struct {
+    char *nombre;
+    char *cuerpo;
+} Macro;
+
+typedef struct {
+    int elementos;
+    Macro **macros;
+}Tabla_macros;
+
+void guardar_macro(Macro tabla_macros, char *nombre, int num_parametros, char **parametros, char *cuerpo);
