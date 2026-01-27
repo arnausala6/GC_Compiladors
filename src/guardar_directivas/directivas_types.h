722cbd1c26e4b7b7c68bf06d2c073f8b7272803c
Wassim Ibn Tahar <156601472+wassimibn@users.noreply.github.com>
2026-01-27 20:17:07 +0100

    header dirrectivas_types.h
    Added header comments to describe the purpose and structure of the directivas_types.h file.

--- src/guardar_directivas/directivas_types.h
+++ src/guardar_directivas/directivas_types.h
@@ -1,3 +1,25 @@
+/*
+* -----------------------------------------------------------------------------
+* Programa: P1PP – Preprocesador
+* Archivo: directivas_types.h
+* Autor: Wassim Ibn Tahar
+* Fecha: 18/01/2026
+*
+* Descripción:
+* Este archivo define los tipos de datos usados para representar directivas del
+* preprocesador de forma estructurada. Incluye:
+*   - DirKind: enumeración con los tipos de directiva soportados (#define, #include,
+*     #ifdef, #endif) y un tipo DIR_UNKNOWN para cualquier otra directiva.
+*   - Directiva: estructura que almacena el tipo de directiva, su localización
+*     (SrcLoc) y los datos asociados mediante una unión para cada caso
+*     (nombre/valor, ruta de include, símbolo de ifdef o texto crudo).
+*     Hacemos union ya que la directiva solo puede ser de un tipo a la vez y optimizamos el uso de memoria
+*
+* También declara la función directiva_free(), encargada de liberar la memoria
+* dinámica asociada a una Directiva
+* -----------------------------------------------------------------------------
+*/
+
 #ifndef DIRECTIVAS_TYPES_H
 #define DIRECTIVAS_TYPES_H
 
