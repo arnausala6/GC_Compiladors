94e70c493e0f7bad9ed2525dbf777e89f91b77de
Wassim Ibn Tahar <156601472+wassimibn@users.noreply.github.com>
2026-02-15 17:50:11 +0100

    Update output_writer.h

--- src/output_writer/output_writer.h
+++ src/output_writer/output_writer.h
@@ -1,1 +1,25 @@
-#
+#ifndef OUTPUT_WRITER_H
+#define OUTPUT_WRITER_H
+
+#include <stdio.h>
+#include "token_model.h"
+
+typedef enum {
+    OUT_RELEASE = 0,
+    OUT_DEBUG   = 1
+} OutFormat;
+
+typedef struct {
+    FILE *output;            /* stream de salida (lo abre/cierra el driver) */
+    OutFormat format;        /* release o debug */
+    int debug_on;            
+    int last_line_written;   /* para sincronizar líneas del input -> output */
+} OutputWriter;
+
+void output_writer_init(OutputWriter *ow, FILE *out, OutFormat format, int debug_on);
+
+void output_writer_write_token(OutputWriter *ow, const Token *tok);
+
+void output_writer_close(OutputWriter *ow);
+
+#endif
