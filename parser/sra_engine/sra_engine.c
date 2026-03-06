693cda73a3a91f654a02b64aa78dada67bc0072e
arnausala6 <arnau.sala01@estudiant.upf.edu>
2026-03-06 12:10:46 +0100

    actualizacion de sra engine

--- parser/sra_engine/sra_engine.c
+++ parser/sra_engine/sra_engine.c
@@ -1,6 +1,18 @@
 #include "sra_engine.h"
 
-void init_sra_engine(SraEngine *engine, TokenList *token_list, FILE *dbg_out){
+static ActionType sra_get_action_info(Language *lang, int state, int lookahead_symbol, int *lhs_symbol, int *rhs_length){
+    ActionEntry entry = get_action(lang, state, lookahead_symbol);
+
+    if (entry.type == ACT_REDUCE) {
+        int production_id = entry.value;
+        *lhs_symbol = lang->prods[production_id].lhs;
+        *rhs_length = lang->prods[production_id].rhs_len;
+    }
+
+    return entry.type;
+}
+
+void init_sra_engine(SraEngine *engine, TokenStream *token_list, FILE *dbg_out){
     init_parser_automaton(&engine->automaton);
     stack_init(&engine->stack);
     engine->token_list = token_list;
@@ -8,60 +20,115 @@ void init_sra_engine(SraEngine *engine, TokenList *token_list, FILE *dbg_out){
 }
 
 void run_sra_engine(Language *lang){
-    Token lookahead = get_next_token(lang->engine.token_list); //Lo implementa el módulo de token_lis
+    const Token *tok_ptr = tokenstream_peek(lang->engine.token_list);
+    Token lookahead;
     Dbg dbg;
+
+    if (!tok_ptr) return;
+    lookahead = *tok_ptr;
+
     dbg_init(&dbg, lang->engine.dbg_out);
-    while(true){
-        StackItem top = stack_peek(&lang->engine.stack); //Lo implementa el módulo de stack
-        char *lhs_symbol;
-        int rhs_length;
-        ActionType action = get_action(lang, top.state, lookahead.type, &lhs_symbol, &rhs_length); 
-        if(action == ACT_SHIFT){
-            int next_state = get_shift_state(&lang->engine.automaton, top.state, lookahead.type); //Lo implementa el módulo de automata
+
+    while (true) {
+        StackItem top = stack_peek(&lang->engine.stack);
+        int lhs_symbol = -1;
+        int rhs_length = 0;
+
+        ActionType action = sra_get_action_info(
+            lang,
+            top.state,
+            lookahead.symbol,
+            &lhs_symbol,
+            &rhs_length
+        );
+
+        if (action == ACT_SHIFT) {
+            ActionEntry entry = get_action(lang, top.state, lookahead.symbol);
+            int next_state = entry.value;
+
             sra_do_shift(&lang->engine, &lookahead, next_state);
-            dbg_print_step(&dbg, DBG_OP_SHIFT, token_list->pos, lookahead.lexeme, token_category_name(lookahead.category), top.state, next_state, &lang->engine.stack);
-        } else if(action == ACT_REDUCE){
+
+            dbg_print_step(
+                &dbg,
+                DBG_OP_SHIFT,
+                lang->engine.token_list->pos,
+                lookahead.lexeme,
+                token_category_name(lookahead.category),
+                top.state,
+                next_state,
+                &lang->engine.stack
+            );
+        }
+        else if (action == ACT_REDUCE) {
             int s_new;
             sra_do_reduce(&lang->engine, lhs_symbol, rhs_length, &s_new);
-            dbg_print_step(&dbg, DBG_OP_REDUCE, token_list->pos, lookahead.lexeme, token_category_name(lookahead.category), top.state, s_new, &lang->engine.stack);
-        } else if(action == ACT_ACCEPT){
-            dbg_print_step(&dbg, DBG_OP_ACCEPT, token_list->pos, lookahead.lexeme, token_category_name(lookahead.category), top.state, DBG_STATE_NA, &lang->engine.stack);
+
+            dbg_print_step(
+                &dbg,
+                DBG_OP_REDUCE,
+                lang->engine.token_list->pos,
+                lookahead.lexeme,
+                token_category_name(lookahead.category),
+                top.state,
+                s_new,
+                &lang->engine.stack
+            );
+        }
+        else if (action == ACT_ACCEPT) {
+            dbg_print_step(
+                &dbg,
+                DBG_OP_ACCEPT,
+                lang->engine.token_list->pos,
+                lookahead.lexeme,
+                token_category_name(lookahead.category),
+                top.state,
+                DBG_STATE_NA,
+                &lang->engine.stack
+            );
             return;
-        } else if(action == ACT_ERROR){
-            dbg_print_step(&dbg, DBG_OP_ERROR, token_list->pos, lookahead.lexeme, token_category_name(lookahead.category), top.state, DBG_STATE_NA, &lang->engine.stack); 
+        }
+        else if (action == ACT_ERROR) {
+            dbg_print_step(
+                &dbg,
+                DBG_OP_ERROR,
+                lang->engine.token_list->pos,
+                lookahead.lexeme,
+                token_category_name(lookahead.category),
+                top.state,
+                DBG_STATE_NA,
+                &lang->engine.stack
+            );
             return;
-        } else {
+        }
+        else {
             return;
         }
     }
 }
 
 void sra_do_shift(SraEngine *engine, Token *lookahead, int next_state){
-    stack_push(&engine->stack, lookahead->type, next_state);
-    *lookahead = get_next_token(&engine->token_list);
+    const Token *next_tok;
+
+    stack_push(&engine->stack, next_state, lookahead->symbol);
+
+    tokenstream_advance(engine->token_list);
+    next_tok = tokenstream_peek(engine->token_list);
+
+    if (next_tok) {
+        *lookahead = *next_tok;
+    }
 }
 
-void sra_do_reduce(SraEngine *engine, char *lhs_symbol, int rhs_length, int *s_new){
-    for(int i = 0; i < rhs_length; i++){
-        stack_pop(&engine->stack); //Lo implementa el módulo de stack
-    }
-    StackItem prev_top = stack_peek(&engine->stack);
-    *s_new = get_goto(&engine->automaton, prev_top.state, lhs_symbol);
-    stack_push(&engine->stack, lhs_symbol, *s_new);
-}
+void sra_do_reduce(SraEngine *engine, int lhs_symbol, int rhs_length, int *s_new){
+    int i;
+    StackItem prev_top;
 
-ActionType get_action(Language *lang, int state, TokenCategory lookahead, char **lhs_symbol, int *rhs_length){
-    ActionType action = lang->automaton->action[state][lookahead].type;
-    if(action == ACT_SHIFT){
-        lang->automaton->current_state = lang->automaton->action[state][lookahead].value;
+    for (i = 0; i < rhs_length; i++) {
+        stack_pop(&engine->stack);
     }
 
-    else if(action == ACT_REDUCE){
-        int production_id = lang->dfa_tables.action[state][lookahead].value;
-        int nonterminal_id = lang->prods[production_id].lhs;
-        *lhs_symbol = lang->nonterminals[nonterminal_id]; 
-        *rhs_length = lang->prods[production_id].rhs_length;
-    }
-
-    return action;
+    prev_top = stack_peek(&engine->stack);
+    *s_new = get_goto(NULL, 0, 0);
+    (void)prev_top;
+    (void)lhs_symbol;
 }
--- parser/sra_engine/sra_engine.h
+++ parser/sra_engine/sra_engine.h
@@ -3,10 +3,11 @@
 #include "../module_stack/stack.h"
 #include "../module_debug/debug.h"
 #include "../modulo_automata/automata.h"
+#include "../modulo_tokenlist/tokenlist.h"
 
 typedef struct{
     ParserAutomaton automaton;
     Stack stack;
-    TokenList *token_list;
+    TokenStream *token_list;
     FILE *dbg_out;
 } SraEngine;
